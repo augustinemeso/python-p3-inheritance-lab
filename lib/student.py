9d33902205bb4ec4f9bcdccb6b61625c81b309de
augustinemeso <augustinemesobk@gmail.com>
2024-12-07 16:01:37 +0300

    add updated PYTHON-P3-INHERITANCE-LAB

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
-
+# student.py
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Call parent class initializer
+        self.knowledge = []  # Initialize with an empty list for knowledge
     
-    def learn(self):
-        pass
+    def learn(self, knowledge_string):
+        # Add a knowledge string to the student's list of knowledge
+        self.knowledge.append(knowledge_string)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,21 @@
-#!/usr/bin/env python
-
+# teacher.py
+import random
 from user import User
 
-import random
-
 class Teacher(User):
-
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Call parent class initializer
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
+    
     def teach(self):
-        pass
+        # Return a random knowledge item from the list
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
